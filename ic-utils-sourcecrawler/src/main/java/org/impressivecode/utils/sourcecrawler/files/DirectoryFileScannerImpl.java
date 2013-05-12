2b783804cefdbfebbe0bf311dbe68459a766dda2
paweln1986 <Pawe Nosal>
2013-05-12 17:24:41 +0200

    first commit

---
+++ ic-utils-sourcecrawler/src/main/java/org/impressivecode/utils/sourcecrawler/files/DirectoryFileScannerImpl.java
@@ -0,0 +1,54 @@
+/*
+ ImpressiveCode Depress Framework Source Crawler
+ Copyright (C) 2013 ImpressiveCode contributors
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.impressivecode.utils.sourcecrawler.files;
+
+/**
+ *
+ * @author Paweł Nosal
+ *
+ */
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.File;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+public class DirectoryFileScannerImpl implements DirectoryFileScanner {
+
+	private static final String NULL_POINTER_EXCEPTION_MSG = "given file should not be null";
+
+	public List<File> getFilesByDirectory(final File directory) {
+		checkNotNull(directory, NULL_POINTER_EXCEPTION_MSG);
+		FileHelper.checkIsDirectory(directory);
+		return getListOfFilesFromDirectory(directory);
+	}
+
+	private List<File> getListOfFilesFromDirectory(final File directory) {
+		List<File> returnList;
+		final File[] listFiles = directory.listFiles();
+		if (listFiles == null) {
+			returnList = Lists.newArrayList();
+		} else {
+			returnList = Lists.newArrayList(listFiles);
+		}
+		return returnList;
+	}
+
+}
