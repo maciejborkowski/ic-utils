70ede833c09179604c955659dcb5e2f6036c2647
paweln1986 <Pawe Nosal>
2013-05-14 23:42:19 +0200

    FileScanner full implementation. FileProcessor full implementation. FileHelper full implementation.

---
+++ ic-utils-sourcecrawler/src/main/java/org/impressivecode/utils/sourcecrawler/files/FileScannerImpl.java
@@ -0,0 +1,53 @@
+/*
+ImpressiveCode Depress Framework Source Crawler
+Copyright (C) 2013 ImpressiveCode contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.impressivecode.utils.sourcecrawler.files;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+import static com.google.common.base.Preconditions.*;
+
+/**
+ * 
+ * @author Paweł Nosal
+ * 
+ */
+
+public class FileScannerImpl implements FileScanner {
+	private FileHelper fileHelper;
+	public FileScannerImpl(FileHelper fileHelper) {
+		this.fileHelper = fileHelper;
+	}
+
+	@Override
+	public List<Path> scanDirectoryFiles(Path path, FileProcessor fileProcessor) throws IOException {
+		checkNotNull(path,"Path should not be null.");
+		checkNotNull(fileProcessor, "FileProcessor should not be null.");
+		checkIsDirectory(path);
+		fileHelper.walkFiles(path, fileProcessor);
+		return fileProcessor.getPaths();
+	}
+
+	private void checkIsDirectory(Path path) {
+		if(!fileHelper.isDirectory(path)){
+			throw new IllegalArgumentException("File from path should be directory.");
+		}
+	}
+
+}
