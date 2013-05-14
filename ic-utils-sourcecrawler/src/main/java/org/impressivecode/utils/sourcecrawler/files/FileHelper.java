c3a1f738da287f3f8bd645052a1ba9b95bdee3f0
paweln1986 <Pawe Nosal>
2013-05-14 23:40:12 +0200

    FileScanner full implementation

--- ic-utils-sourcecrawler/src/main/java/org/impressivecode/utils/sourcecrawler/files/FileHelper.java
+++ ic-utils-sourcecrawler/src/main/java/org/impressivecode/utils/sourcecrawler/files/FileHelper.java
@@ -1,36 +1,37 @@
 /*
- ImpressiveCode Depress Framework Source Crawler
- Copyright (C) 2013 ImpressiveCode contributors
+ImpressiveCode Depress Framework Source Crawler
+Copyright (C) 2013 ImpressiveCode contributors
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.impressivecode.utils.sourcecrawler.files;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
 
 /**
  *
  * @author Paweł Nosal
  *
  */
-import java.io.File;
 
-public final class FileHelper {
-	private static final String FILE_IS_NOT_DIRECTORY_MSG = "given file should be a directory, but he wasn't";
+public interface FileHelper {
 
-	public static void checkIsDirectory(final File directory) {
-		final boolean isDirectory = directory.isDirectory();
-		if (!isDirectory) {
-			throw new IllegalArgumentException(FILE_IS_NOT_DIRECTORY_MSG);
-		}
-	}
+	boolean isDirectory(Path path);
+
+	Path walkFiles(Path path, FileProcessor fileProcessor) throws IOException;
+	
+	PathMatcher getPathMatcher(String regExp);
 }
--- ic-utils-sourcecrawler/src/test/java/org/impressivecode/utils/sourcecrawler/FirstTest.java
+++ ic-utils-sourcecrawler/src/test/java/org/impressivecode/utils/sourcecrawler/FirstTest.java
@@ -1,9 +1,33 @@
 package org.impressivecode.utils.sourcecrawler;
 
+import static org.fest.assertions.Assertions.assertThat;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.impressivecode.utils.sourcecrawler.files.FileHelper;
+import org.impressivecode.utils.sourcecrawler.files.FileHelperImpl;
+import org.impressivecode.utils.sourcecrawler.files.FileProcessor;
+import org.impressivecode.utils.sourcecrawler.files.FileProcessorImpl;
+import org.impressivecode.utils.sourcecrawler.files.FileScanner;
+import org.impressivecode.utils.sourcecrawler.files.FileScannerImpl;
 import org.testng.annotations.Test;
 
 public class FirstTest {
-  @Test
-  public void f() {
-  }
+	@Test
+	public void f() throws IOException {
+		String ROOT = ".";
+		Path path = Paths.get(ROOT);
+		FileHelper fileHelper = new FileHelperImpl();
+		PathMatcher matcher = fileHelper.getPathMatcher("glob:*.java");
+		FileScanner fileScanner = new FileScannerImpl(fileHelper);
+		List<Path> list = new ArrayList<Path>();
+		FileProcessor fileProcessor = new FileProcessorImpl(list, matcher);
+		fileScanner.scanDirectoryFiles(path, fileProcessor);
+		assertThat(list).isNotEmpty();
+	}
 }
