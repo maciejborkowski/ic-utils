af436d5414fcc67901bac23fd1cff13a2624eec8
paweln1986 <Pawe Nosal>
2013-05-28 21:42:13 +0200

    Java source analyzer

---
+++ ic-utils-sourcecrawler/src/main/java/org/impressivecode/utils/sourcecrawler/model/JavaClass.java
@@ -0,0 +1,64 @@
+/*
+ImpressiveCode Depress Framework Source Crawler
+Copyright (C) 2013 ImpressiveCode contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.impressivecode.utils.sourcecrawler.model;
+
+/**
+ * 
+ * @author Paweł Nosal
+ * 
+ */
+
+public class JavaClass {
+	private String className;
+	private ClazzType classType;
+	private boolean isException;
+	private boolean isTest;
+
+	public String getClassName() {
+		return className;
+	}
+
+	public ClazzType getClassType() {
+		return classType;
+	}
+
+	public boolean isException() {
+		return isException;
+	}
+
+	public boolean isTest() {
+		return isTest;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public void setClassType(ClazzType classType) {
+		this.classType = classType;
+	}
+
+	public void setException(boolean isException) {
+		this.isException = isException;
+	}
+
+	public void setTest(boolean isTest) {
+		this.isTest = isTest;
+	}
+
+}
--- ic-utils-sourcecrawler/src/test/java/org/impressivecode/utils/sourcecrawler/FirstTest.java
+++ ic-utils-sourcecrawler/src/test/java/org/impressivecode/utils/sourcecrawler/FirstTest.java
@@ -2,6 +2,7 @@ package org.impressivecode.utils.sourcecrawler;
 
 import static org.fest.assertions.Assertions.assertThat;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
@@ -17,6 +18,10 @@ import org.impressivecode.utils.sourcecrawler.files.FileScanner;
 import org.impressivecode.utils.sourcecrawler.files.FileScannerImpl;
 import org.testng.annotations.Test;
 
+import com.thoughtworks.qdox.JavaDocBuilder;
+import com.thoughtworks.qdox.model.JavaClass;
+import com.thoughtworks.qdox.model.JavaSource;
+
 public class FirstTest {
 	@Test
 	public void f() throws IOException {
@@ -29,5 +34,22 @@ public class FirstTest {
 		FileProcessor fileProcessor = new FileProcessorImpl(list, matcher);
 		fileScanner.scanDirectoryFiles(path, fileProcessor);
 		assertThat(list).isNotEmpty();
+		JavaDocBuilder builder = new JavaDocBuilder();
+		for (Path path2 : list) {
+			builder.addSource(path2.toFile());
+		}
+		
+		JavaSource[] javaSources = builder.getSources();
+		for (JavaSource javaSource : javaSources) {
+			
+			System.out.println(javaSource.getPackage());
+			JavaClass[] classes = javaSource.getClasses();
+			for (JavaClass javaClass : classes) {
+				System.out.println("    "+javaClass.getName());
+				System.out.println("    "+javaClass.isInterface());
+				System.out.println("    "+javaClass.isEnum());
+			}
+			
+		}
 	}
 }
